78a5210b295a1f01bbc6a7cf7c7dd781ff033913
Joseph Gerardot <indyJAG@gmail.com>
2019-05-09 17:46:29 +0000

    Initialize the project

---
+++ readme.md
@@ -0,0 +1,4 @@
+# JAG Budget
+This is my personal budget and expense analysis + tracking tool. If it becomes really good
+other people might consider using it.
+
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+extern crate serde;
+extern crate serde_derive;
+extern crate toml;
+extern crate clap;
+
+use serde::{Serialize, Deserialize};
+use serde_derive::{Serialize, Deserialize};
+use clap::{Arg, App, Subcommand};
+
+fn main() {
+    println!("Hello, world!");
+}
