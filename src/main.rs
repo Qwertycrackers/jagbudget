e047794bb35a763af0329f15d40ea71e198e29a7
Joseph Gerardot <indyJAG@gmail.com>
2019-05-10 14:42:31 +0000

    Set up plan for project in comments

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,23 @@ extern crate clap;
 
 use serde::{Serialize, Deserialize};
 use serde_derive::{Serialize, Deserialize};
-use clap::{Arg, App, Subcommand};
+use clap::{Arg, App, SubCommand};
 
 fn main() {
-    println!("Hello, world!");
+  // Parse args
+  // TODO
+
+  // TODO: Cache as an SQLite DB?
+
+  // Ingest expense records
+  // TODO
+
+  // Ingest income records
+  // TODO
+
+  // Ingest reporting configs (such as budgetary allocation numbers, spendings targets, etc)
+  // TODO
+
+  // Produce report
+  // TODO
 }
